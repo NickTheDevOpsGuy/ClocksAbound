3275f67570c071604bd0e6ea9f94315356f45c93
Nick Clark <limeman@gmail.com>
2025-11-07 18:06:21 -0500

    style: auto-format with Prettier [skip-precheck]

--- src/app/lib/timezones.ts
+++ src/app/lib/timezones.ts
@@ -1,14 +1,14 @@
 export const allTimeZones: string[] = (Intl as any).supportedValuesOf?.(
-  "timeZone",
+  'timeZone'
 ) ?? [
-  "UTC",
-  "America/New_York",
-  "America/Chicago",
-  "America/Denver",
-  "America/Los_Angeles",
-  "Europe/London",
-  "Europe/Paris",
-  "Asia/Tokyo",
-  "Asia/Kolkata",
-  "Australia/Sydney",
+  'UTC',
+  'America/New_York',
+  'America/Chicago',
+  'America/Denver',
+  'America/Los_Angeles',
+  'Europe/London',
+  'Europe/Paris',
+  'Asia/Tokyo',
+  'Asia/Kolkata',
+  'Australia/Sydney',
 ];
